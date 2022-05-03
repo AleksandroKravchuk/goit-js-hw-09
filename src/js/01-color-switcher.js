a9dfd6dd83e0f3fecf31177a3a05db807505f6d4
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-05-03 13:30:33 +0300

    dz9/1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+refs = {
+    btnStart: document.querySelector("button[data-start]"),
+    btnStop: document.querySelector("button[data-stop]"),
+    body: document.querySelector('body'),
+}
 
+
+refs.btnStart.addEventListener('click', (evt) => {
+    evt.currentTarget.setAttribute('disabled', '');
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    
+    // elem.setAttribute(name, value) 
+});
+
+refs.btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+refs.btnStart.removeAttribute('disabled', '');})
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
