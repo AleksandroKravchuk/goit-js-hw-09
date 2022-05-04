87b6c9323e3f908696abe7dcc1dc5fcbdd8e4842
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-05-04 10:01:21 +0300

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,63 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+    onInput: document.querySelector("#datetime-picker"), 
+    btnInput: document.querySelector("button"), 
+    timer: document.querySelector(".timer"),
+    valueTimer: document.querySelector(".value"),
+}
+
+console.log(refs.onInput);
+console.log(refs.btnInput);
+console.log(refs.timer);
+console.log(refs.valueTimer);
+
+refs.btnInput.setAttribute('disabled', '');
+
+// flatpickr(element, {});
 
 
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
 
+        refs.onInput.addEventListener('input', (evt) => {
+            
+            console.log(evt.currentTarget.value)
+        const addValue = evt.currentTarget.value;
+        const date = new Date(addValue);
+            
+            // const currentData = selectedDates[0];
+            // const cur = new Date(currentData);
+       
+            if (date.getTime() < new Date().getTime()) {
+                refs.btnInput.setAttribute('disabled', '');
+                return alert('Ошибка');
+                
+            }
+            // selectedDates = addValue;
+            // const selYear = [...selectedDates]
+            // console.log(selectedDates[data.minute]);
+            refs.btnInput.removeAttribute('disabled');
+           refs.btnInput.classList.add('active');
+    // console.log(evt.currentTarget.value)
+            
+            
+            
+})
 
+        
+      
+  },
+};
 
-
-
-
-
-
-
-
-
-
+flatpickr("#datetime-picker", options);
+// flatpickr.
 
 
 //  if (!title || !task) return notyf.error('Please fill in all fields');
